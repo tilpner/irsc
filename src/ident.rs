ce4ab4d6a395bbce37531225d506552d6a910f3f
James Ravenscroft <ravenscroftj@gmail.com>
2014-12-25 11:39:05 +0000

    Added unwrap calls around ident regex matches

--- src/ident.rs
+++ src/ident.rs
@@ -16,9 +16,9 @@ impl Ident {
             None => return None
         };
         Some(Ident {
-            nickname: c.at(1).into_string(),
-            user: c.at(2).into_string(),
-            host: c.at(3).into_string()
+            nickname: c.at(1).unwrap_or("").into_string(),
+            user: c.at(2).unwrap_or("").into_string(),
+            host: c.at(3).unwrap_or("").into_string()
         })
     }
 }
